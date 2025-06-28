1b266617b03a9efb366dfced4da11157c82a9a4e
Prerna <prernasalya07@gmail.com>
2025-06-28 18:46:43 +0530

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,18 @@
+print("Welcome to the world of Madlibs adventure!")
+
+adjective = input("Enter an adjective(description): ")
+name = input("Enter a name: ")
+object1 = input("Enter an object: ")
+superpower = input("Enter a superpower: ")
+chemical_name = input("Enter a chemical name: ")
+colour = input("Enter a colour: ")
+verb = input("Enter a verb: ")
+pronouns = input("Enter a pronouns: ")
+
+print("Here is your story:")
+
+print(f'''Deep inside the {adjective} laboratory of Dr. {name}, something was bubbling inside a giant {object1}.
+"I have almost cracked the code to {superpower}," whispered Dr. {name}. 
+But just as the final {chemical_name} was added, alarms went off. 
+Within seconds a cloud of {colour} smoke filled the lab and the floor started to {verb}.
+That is when Dr. {name} realized that {pronouns} have failed.''')
